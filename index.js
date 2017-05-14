bda535c18e6204ecfca13953d6e941cb64b378f2
Moon <moon.ghost.jian@gmail.com>
2017-05-14 10:42:05 +0800

    Add new demos.

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ angular.module('demo1', [])
   var vm = this;
 
   vm.info = '啊哈';
+  vm.infoImage = {
+    id: 'img-id',
+    name: 'img-name'
+  };
+  vm.infoText = {
+    id: 'txt-id',
+    name: 'txt-name'
+  };
 })
 .component('dm1', {
   // templateUrl: 'http://7xqxox.com1.z0.glb.clouddn.com/demo.tpl.html',
@@ -54,6 +62,73 @@ angular.module('demo1', [])
     // },
     link: function(scope, element) {
       console.log(11111);
+    }
+  };
+})
+.component('tplImage', {
+  templateUrl: 'image.tpl.html',
+  // template: '<div>这是Demo2 {{ ::comp.p }}</div>',
+  controller: 'DmCtrl',
+  controllerAs: 'tpl',
+  bindings: {
+    info: '<'
+  }
+})
+.component('tplText', {
+  templateUrl: 'text.tpl.html',
+  // template: '<div>这是Demo2 {{ ::comp.p }}</div>',
+  controller: 'DmCtrl',
+  controllerAs: 'tpl',
+  bindings: {
+    info: '<'
+  }
+})
+.directive('popup', function($compile) {
+  return {
+    restrict: 'A',
+    // transclude: true,
+    scope: {
+      type: '@',
+      info: '=infoData'
+    },
+    // templateUrl: function(element, attrs) {
+    //   console.log(attrs.type);
+    //
+    //   return attrs.type + '.tpl.html';
+    //   // return 'image.tpl.html';
+    // },
+    // template: function(element, attrs) {
+    //   // return '<tpl-' + attrs.type + '></tpl-' + attrs.type + '>';
+    //   var tpl = '';
+    //   switch (attrs.type) {
+    //     case 'image':
+    //       tpl = '<tpl-image info="demo.infoImage"></tpl-image>';
+    //       break;
+    //     default:
+    //       tpl = '<tpl-text info="demo.infoText"></tpl-text>';
+    //   }
+    //
+    //   return tpl;
+    // }
+    // compile: function(element, attrs) {
+    //   return {
+    //     pre: function(scope, element, attrs) {
+    //       console.log(44, scope.info, attrs.infoData);
+    //       element.attr('info', attrs.infoData);
+    //     }
+    //   };
+    // }
+    link: function(scope, element, attrs) {
+      console.log(88888, attrs.type);
+      var tpl = '';
+      switch (attrs.type) {
+        case 'image':
+          tpl = '<tpl-image info="info"></tpl-image>';
+          break;
+        default:
+          tpl = '<tpl-text info="info"></tpl-text>';
+      }
+      element.append($compile(tpl)(scope));
     }
   };
 });
